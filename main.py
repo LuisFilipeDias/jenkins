8907e6d48cacbcb451688aea50bc15e57ead064b
Luis Filipe Dias <luisfiliperdias@gmail.com>
2016-03-25 21:34:53 +0000

    Get the jobs from jenkins

--- main.py
+++ main.py
@@ -6,7 +6,6 @@
         DTV Research
             2016
 """
-import jenkinsapi
 from jenkinsapi.jenkins import Jenkins
 from database import DB
 
@@ -17,6 +16,16 @@ def main():
 
     :return: 0 on success
     """
+    jenkins = Jenkins('http://localhost:8080/jenkins', 'user', '16794350')
+    keys = jenkins.keys()
+    # jobs = jenkins.get_jobs()
+    for key in keys:
+        job = jenkins.get_job(key)
+        print("\nJob: %s" % job)
+        builds = job.get_build_dict()
+        for b_i in builds:
+            status = job.get_build(b_i).get_status()
+            print("Status of build: %d %s" % (b_i, status))
     return 0
 
 
